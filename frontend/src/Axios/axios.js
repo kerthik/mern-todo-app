a3052a95219eca217b7bad52b7b438353638f97b
kerthik <113448648+kerthik@users.noreply.github.com>
2024-02-21 15:29:26 +0530

    Update axios.js

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const instance = axios.create({
-	baseURL: `http://${process.env.BACKEND_HOST || 'localhost'}:8000/api`,
+	baseURL: `http://${process.env.BACKEND_HOST || 'a423dec6c3f344a2fafbf18eb0dad0a4-274257718.ap-south-1.elb.amazonaws.com'}:8000/api`,
 });
 export default instance;
